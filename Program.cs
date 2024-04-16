ad0cae5eaacc273239dae343caff42d95ae9e0e3
Aldinorasend <aldinorasend@gmail.com>
2024-04-17 06:39:09 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MOD8_1302223071
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
